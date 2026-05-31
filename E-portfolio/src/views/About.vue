107b6e59d17ff0c35601ecde781ff43fc8441f35
Wuzul <thybe.hoys@gmail.com>
2026-05-31 12:51:53 +0200

    Added project structure

--- E-portfolio/src/views/About.vue
+++ E-portfolio/src/views/About.vue
@@ -1,4 +1,4 @@
 <template>
-  <h1 class="text-white text-6xl font-bold">Home</h1>
+  <h1 class="text-white text-6xl font-bold">About</h1>
 </template>
 <script setup lang="ts"></script>
---
+++ E-portfolio/src/views/Blogposts.vue
@@ -0,0 +1,4 @@
+<template>
+  <h1 class="text-white text-6xl font-bold">Blog posts</h1>
+</template>
+<script setup lang="ts"></script>
